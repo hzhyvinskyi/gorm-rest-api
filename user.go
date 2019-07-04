e6a2d64caea4307e76dfbf2ff5c93dd5a2f96621
hzhyvinskyi <hzhyvinskyi@gmail.com>
2019-07-04 23:54:31 +0300

    Added other crud methods for User

--- user.go
+++ user.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"net/http"
 )
 
 var db *gorm.DB
@@ -13,7 +14,7 @@ var err error
 
 type User struct {
 	gorm.Model
-	Name string
+	Name  string
 	Email string
 }
 
@@ -42,13 +43,60 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func addUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "New user...")
+	db, err := gorm.Open("sqlite3", "appdb")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Cannot connect to the database")
+	}
+	defer db.Close()
+
+	var vars = mux.Vars(r)
+	name := vars["name"]
+	email := vars["email"]
+
+	db.Create(&User{Name: name, Email: email})
+
+	fmt.Fprintf(w, "User was created successfully")
 }
 
 func updateUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Updated user...")
+	db, err := gorm.Open("sqlite3", "appdb")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Cannot connect to the database")
+	}
+	defer db.Close()
+
+	var vars = mux.Vars(r)
+	name := vars["name"]
+	email := vars["email"]
+
+	var user User
+
+	db.Where("name = ?", name).Find(&user)
+
+	user.Email = email
+
+	db.Save(&user)
+
+	fmt.Fprintf(w, "User was successfully updated")
 }
 
 func deletedUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Deleted user...")
+	db, err := gorm.Open("sqlite3", "appdb")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Cannot connect to the database")
+	}
+	defer db.Close()
+
+	var vars = mux.Vars(r)
+	name := vars["name"]
+
+	var user User
+
+	db.Where("name = ?", name).Find(&user)
+	db.Delete(&user)
+
+	fmt.Fprintf(w, "User was deleted")
 }
